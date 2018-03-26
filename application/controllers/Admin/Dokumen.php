621f6eb69c04b46519a9c1d2e6dfecbb7e8416f9
aryana373 <aryanadwiputra@gmail.com>
2018-03-26 13:47:41 +0800

    edit dokumen

--- application/controllers/Admin/Dokumen.php
+++ application/controllers/Admin/Dokumen.php
@@ -38,19 +38,42 @@ class Dokumen extends CI_Controller {
 		$id_dokumen = $this->db->insert_id();
 
 		$this->preprocessing($id_dokumen,$deskripsi);
+		$this->index();
+
 		
 	}
 
 	public function update() {
 		
 		$id=$this->input->post('id');
+		$curr_index= $this->M_dokumen->select_AllTermIndex($id);
+		$curr_TFindex=$this->M_dokumen->select_AllTFIndex($id);
+
 		$judul=$this->input->post('judul');
 		$deskripsi= $this->input->post('deskripsi');
 		$langkah=$this->input->post('langkah');
 		$tgl_edit = date("Y-m-d");
 		$this->M_dokumen->update($id, $judul, $deskripsi, $langkah, $tgl_edit);
+		$this->M_dokumen->deleteIndex($id);
+
+		//membuat index baru
+		$this->preprocessing($id,$deskripsi);
+		$new_index= $this->M_dokumen->select_AllTermIndex($id);
+		$new_TFindex=$this->M_dokumen->select_AllTFIndex($id);
+		$jum_term_index=count($curr_index);
+		
+		//hapus term
+		for ($i = 0; $i < $jum_term_index; $i++)
+        {
+			$this->M_dokumen->subtractTFTerm($curr_index[$i],$curr_TFindex[$i]);
+		}
+
+
+
+
 		$this->index();
 	}
+
 	public function addLangkah() {
 		
 		$id=$this->input->post('id');
@@ -65,12 +88,12 @@ class Dokumen extends CI_Controller {
 	public function delete($id){
 		$term_tersimpan_index = $this->M_dokumen->select_AllTermIndex($id);
 		$tf_tersimpan_index = $this->M_dokumen->select_AllTFIndex($id);
-		$term_tersimpan = $this->M_dokumen->select_AllTerm();
+		
 		$jum_term_index=count($term_tersimpan_index);
 
 		for ($i = 0; $i < $jum_term_index; $i++)
         {
-
+        $term_tersimpan = $this->M_dokumen->select_AllTerm();
 		if (in_array($term_tersimpan_index[$i], $term_tersimpan)) {
         		$this->M_dokumen->subtractTFTerm($term_tersimpan_index[$i],$tf_tersimpan_index[$i]);
         	}
@@ -191,7 +214,7 @@ class Dokumen extends CI_Controller {
         	
 			
 		}
-		$this->index();
+		//$this->index();
 
 	}
 
